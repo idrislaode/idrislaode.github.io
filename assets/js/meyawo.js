3a272b8261c3b560ba8772c9f9c041e8f5732fc0
La Ode Muhamad Idris <lmidris19@gmail.com>
2025-01-14 21:51:44 +0700

    update

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -13,6 +13,20 @@
 */
 
 // smooth scroll
+
+document.addEventListener("DOMContentLoaded", function() {
+    let titleText = "Idrislaode - Robotics AI IoT Enthusiast | ";
+    let position = 0;
+
+    function scrollTitle() {
+        document.title = titleText.substring(position) + titleText.substring(0, position);
+        position = (position + 1) % titleText.length;
+        setTimeout(scrollTitle, 200); // Sesuaikan kecepatan scrolling (dalam ms)
+    }
+
+    scrollTitle();
+});
+
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
